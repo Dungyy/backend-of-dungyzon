fc5f74ff2aef0b70df941c97377034fa6b1a8bd9
Dungyy <erickmunoz13@gmail.com>
2023-03-18 15:37:04 -0500

    backend update

--- index.js
+++ index.js
@@ -15,10 +15,9 @@ require("dotenv").config();
 app.use((req, res, next) => {
   res.setHeader("Access-Control-Allow-Origin", "*");
   res.setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PATCH, DELETE, OPTIONS");
+  res.setHeader("Access-Control-Allow-Methods", "GET");
   next();
 });
-
 
 app.use(express.json());
 
