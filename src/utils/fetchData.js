1b8ec5debb77a4e7bb26c386daa56764fd57b94b
Erick <erickmunoz13@gmail.com>
2024-01-30 02:37:17 -0600

    cleaned abit

--- src/utils/fetchData.js
+++ src/utils/fetchData.js
@@ -1,21 +1,11 @@
 import request from 'request-promise';
-import { setCache, getCache } from './cache.js';
 import logger from '../logger.js';
 
 const fetchData = async (url) => {
   try {
-    const cachedData = getCache(url);
-    if (cachedData) {
-      logger.debug(`Cache hit for URL: ${url}`);
-      logger.info(`Using Cache data`);
-      return cachedData;
-    }
-
-    logger.debug(`Cache miss for URL: ${url}`);
-    logger.info(`Cache miss, adding data to cache`);
+    logger.debug(`Fetching data for URL: ${url}`);
     const response = await request(url);
     const data = JSON.parse(response);
-    setCache(url, data);
     return data;
   } catch (error) {
     logger.error(`Error in fetchData for URL ${url}:`, error);
