8d7af15b1c9694db5b7c444146ecd86a9fc2322f
Erick <erickmunoz13@gmail.com>
2024-02-03 03:01:23 -0600

    added rate limit

--- src/global.js
+++ src/global.js
@@ -13,3 +13,6 @@ export const IS_DEV = process.env.NODE_ENV === 'development';
 export const PORT = IS_DEV ? 5000 : process.env.PORT;
 
 export const BASE_URL = `http://api.scraperapi.com?api_key=${process.env.API_KEY}&autoparse=true`;
+
+export const LIMIT_MAX = 10
+export const LIMIT_TIMEOUT_MAX = parseInt(process.env.LIMIT_TIMEOUT_SET, 10);
--- src/index.js
+++ src/index.js
@@ -2,13 +2,22 @@ import express from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
 import router from './routes/scraperRoutes.js';
-import { PORT } from './global.js';
+import { PORT, LIMIT_TIMEOUT_MAX, LIMIT_MAX } from './global.js';
 import logger from './logger.js';
+import rateLimit from 'express-rate-limit';
 
 dotenv.config();
 
+const limiter = rateLimit({
+  windowMs: LIMIT_TIMEOUT_MAX, // xx minutes
+  max: LIMIT_MAX, // Limit each IP to xx requests per windowMs
+  standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
+  legacyHeaders: false, // Disable the `X-RateLimit-*` headers
+});
+
 const app = express();
 app.use(cors());
+app.use(limiter);
 app.use(express.json());
 app.use('/', router);
 
