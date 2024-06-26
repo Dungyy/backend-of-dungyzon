14b120051c01dd9e3c7ca411855944cd466504f0
Erick <erickmunoz13@gmail.com>
2024-06-26 01:04:35 -0500

    minor change to global

--- src/global.js
+++ src/global.js
@@ -2,7 +2,7 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 
-export const API_KEY = process.env.API_KEY;
+const API_KEY = process.env.API_KEY;
 
 export const IS_PROD = !(
   process.env.NODE_ENV === 'development' || process.env.NODE_ENV === 'test'
@@ -12,7 +12,7 @@ export const IS_DEV = process.env.NODE_ENV === 'development';
 
 export const PORT = IS_DEV ? 5000 : process.env.PORT;
 
-export const BASE_URL = `http://api.scraperapi.com?api_key=${process.env.API_KEY}&autoparse=true`;
+export const BASE_URL = `http://api.scraperapi.com?api_key=${API_KEY}&autoparse=true`;
 
 export const LIMIT_MAX = 10
 export const LIMIT_TIMEOUT_MAX = parseInt(process.env.LIMIT_TIMEOUT_SET, 10);
